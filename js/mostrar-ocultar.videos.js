8d0ffd1fd573bab1e84f598ca206ac25f033df79
fandubvarios <84653619+fandubvarios@users.noreply.github.com>
2021-05-22 23:12:46 -0600

    Add files via upload

---
+++ js/mostrar-ocultar.videos.js
@@ -0,0 +1,25 @@
+function mostrarMega() {
+    document.getElementById('Mega').style.display="block";
+    document.getElementById('Okru').style.display="none";
+    document.getElementById('MixDrop').style.display="none";
+
+}
+function mostrarOkru() {
+    document.getElementById('Mega').style.display="none";
+    document.getElementById('Okru').style.display="block";
+    document.getElementById('MixDrop').style.display="none";
+}
+function mostrarMixDrop() {
+    document.getElementById('Mega').style.display="none";
+    document.getElementById('Okru').style.display="none";
+    document.getElementById('MixDrop').style.display="block";
+}
+/*
+    La banda dice que yorokobi no se baña :n
+    Pd: Le hice unos cambios a la páina de sinopsis de TGK
+    Probablemente también le haga cambios a las entradas de cada capítulo
+    He notado que hace falta un modo para navegar entre capítulos o regresar al anterior.
+
+    Es todo, siga en lo suyo buen hombre :D
+    Atte: Rudy.
+*/
