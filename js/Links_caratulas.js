5b9ceeea0b859a20655b54c6f1c82970c93e5c25
fandubvarios <84653619+fandubvarios@users.noreply.github.com>
2021-08-25 22:04:58 -0600

    Update Links_caratulas.js

--- js/Links_caratulas.js
+++ js/Links_caratulas.js
@@ -1,3 +1,6 @@
 function redireccionarHamefura() {
     location.href="proyectos/Hamefura/SINOPSIS.html";
 }
+function redireccionarKotoura() {
+    location.href="proyectos/KOTOURA-SAN/SINOPSIS.html";
+}
