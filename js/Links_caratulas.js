d5389a6bd0da95e8f55dd6cec8f9f04963b2a929
RfigueroaS <84605785+RfigueroaS@users.noreply.github.com>
2021-08-25 23:28:39 -0500

    Update Links_caratulas.js

--- js/Links_caratulas.js
+++ js/Links_caratulas.js
@@ -1,6 +1,18 @@
 function redireccionarHamefura() {
     location.href="proyectos/Hamefura/SINOPSIS.html";
+    /*
+        Esta función es EXCLUSIVA para su uso en el Index.html
+    */
+}
+function redireccionarHamefura_2() {
+    location.href="../proyectos/Hamefura/SINOPSIS.html";
+     /*
+        Esta función es EXCLUSIVA para su uso en todas las áginas dentro de la carpeta Proyectos
+    */
 }
 function redireccionarKotoura() {
     location.href="proyectos/KOTOURA-SAN/SINOPSIS.html";
+     /*
+        Esta función es EXCLUSIVA para su uso en el Index.html
+    */
 }
