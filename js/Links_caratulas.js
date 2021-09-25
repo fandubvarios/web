5dffc43200f9b752b586526fe82160fc5c7cf11d
RfigueroaS <84605785+RfigueroaS@users.noreply.github.com>
2021-09-25 12:26:17 -0500

    Update Links_caratulas.js

--- js/Links_caratulas.js
+++ js/Links_caratulas.js
@@ -1,60 +1,32 @@
+/*Estas funciones son EXCLUSIVAS para su uso en todas las páginas dentro del index.html*/
 function redireccionarHamefura() {
     location.href="proyectos/Hamefura/SINOPSIS.html";
-    /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
-}
-function redireccionarHamefura_2() {
-    location.href="../proyectos/Hamefura/SINOPSIS.html";
-     /*
-        Esta función es EXCLUSIVA para su uso en todas las áginas dentro de la carpeta Proyectos
-    */
 }
 function redireccionarKotoura() {
     location.href="proyectos/KOTOURA-SAN/SINOPSIS.html";
-     /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
 }
 function redireccionarPhantom() {
-    location.href="#";
-     /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
+    location.href="https://fandubvarios.github.io/web/proyectos/PHANTOM/SINOPSIS.html";
 }
 function redireccionarInuyashiki() {
-    location.href="#";
-     /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
+    location.href="https://fandubvarios.github.io/web/proyectos/INUYASHIKI/SINOPSIS.html";
 }
 function redireccionarLoveLab() {
-    location.href="#";
-     /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
+    location.href="https://fandubvarios.github.io/web/proyectos/LL/SINOPSIS.html";
 }
 function redireccionarSatsuriku() {
-    location.href="#";
-     /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
+    location.href="https://fandubvarios.github.io/web/proyectos/SNT/SINOPSIS.html";
 }
 function redireccionarTokyoGhoul() {
-    location.href="#";
-     /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
+    location.href="https://fandubvarios.github.io/web/proyectos/TG/SINOPSIS.html";
 }
 function redireccionarJunniTaisen() {
-    location.href="#";
-     /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
+    location.href="https://fandubvarios.github.io/web/proyectos/JT/SINOPSIS.html";
 }
 function redireccionarKonosuba() {
-    location.href="#";
-     /*
-        Esta función es EXCLUSIVA para su uso en el Index.html
-    */
+    location.href="https://fandubvarios.github.io/web/proyectos/KONOSUBA/SINOPSIS.html";
+}
+/*Estas funciones son EXCLUSIVAS para su uso en todas las páginas dentro de la carpeta Proyectos*/
+function redireccionarHamefura_2() {
+    location.href="../proyectos/Hamefura/SINOPSIS.html";
 }
